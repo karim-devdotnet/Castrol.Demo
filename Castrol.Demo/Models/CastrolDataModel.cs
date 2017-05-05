17b44cd5a8b101bbbc2f7e125eea5ba2072be768
karimabd84 <abdelkarim.esti@gmail.com>
2017-05-05 08:58:53 +0200

    EVHC - Übersetzungen

--- Castrol.Demo/Models/CastrolDataModel.cs
+++ Castrol.Demo/Models/CastrolDataModel.cs
@@ -11,82 +11,82 @@ namespace Castrol.Demo.Models
     public class CastrolDataModel
     {
 
-        [Required(ErrorMessage = "\"{0}\" ist erforderlich."), Display(Name = "Selected Customer Name*")]
+        [Required(ErrorMessage = "\"{0}\" ist erforderlich."), Display(Name = "Name*")]
         public string CustomerName { get; set; }
 
-        [Display(Name = "Vehicle")]
+        [Display(Name = "Hersteller")]
         public string Vehicle { get; set; }
 
-        [Display(Name = "Model")]
+        [Display(Name = "Modell")]
         public string VehicleModel { get; set; }
 
         /// <summary>
         /// Kennzeichen
         /// </summary>
         [Required(ErrorMessage = "\"{0}\" ist erforderlich.")]
-        [Display(Name = "Registration*")]
+        [Display(Name = "Kennzeichen*")]
         public string Registration { get; set; }
 
         /// <summary>
         /// Work Order Number
         /// </summary>
-        [Display(Name = "Work Order Number")]
+        [Display(Name = "Reihenfolge")]
         public string WIP { get; set; }
 
         [Required(ErrorMessage = "\"{0}\" ist erforderlich.")]
-        [Display(Name = "EVHC Date/Time In*")]
+        [Display(Name = "Datum/Uhrzeit*")]
         public string EVHCDateTimeIn { get; set; }
 
         /// <summary>
         /// Fahrgestellnummer
         /// </summary>
-        [Display(Name = "Vin Number")]
+        [Display(Name = "Fahrgestell-Nr (VIN)")]
         public string VIN { get; set; }
 
-        [Display(Name = "Home Phone")]
+        [Display(Name = "Telefon")]
         public string HomePhone { get; set; }
 
-        [Display(Name = "Mobile")]
+        [Display(Name = "Telefon (Mobil)")]
         public string Mobile { get; set; }
 
-        [Display(Name = "Work Phone")]
+        [Display(Name = "Telefon (Arbeit)")]
         public string WorkPhone { get; set; }
 
-        [Display(Name = "Email")]
+        [Display(Name = "E-Mail")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [Display(Name = "Fax")]
         public string Fax { get; set; }
 
-        [Display(Name = "Sales Advisor (if allocated)")]
+        [Display(Name = "Verkaufsberater")]
         public string SalesAdvisor { get; set; }
 
-        [Display(Name = "Mileage")]
+        [Display(Name = "Kilometerstand")]
         public int Mileage { get; set; }
 
-        [Display(Name = "Date Vehicle 1st Registered")]
+        [Display(Name = "Erstzulassung")]
         public string DateVehicleFirstRegistered { get; set; }
 
-        [Display(Name = "Contact Phone (on the day)")]
+        [Display(Name = "Telefon (heute)")]
         public string ContactPhone { get; set; }
 
-        [Display(Name = "Car Date/Time Due Out")]
+        [Display(Name = "Fertigstellung bis")]
         public string CarDateTimeDueOut { get; set; }
 
-        [Display(Name = "Customer Waiting")]
+        [Display(Name = "Kunde wartet")]
         public int CustomerWaiting { get; set; }
 
         /// <summary>
         /// Kunden ID
         /// </summary>
-        [Required(ErrorMessage ="\"{0}\" ist erforderlich."), Display(Name ="User ID*")]
+        [Required(ErrorMessage ="\"{0}\" ist erforderlich."), Display(Name = "Benutzer-ID*")]
         public string UserID { get; set; }
 
-        [Required(ErrorMessage = "\"{0}\" ist erforderlich."), Display(Name ="FTP username*")]
+        [Required(ErrorMessage = "\"{0}\" ist erforderlich."), Display(Name = "FTP Benutzername*")]
         public string UserName { get; set; }
 
-        [Display(Name ="FTP user password")]
+        [Display(Name = "FTP Passwort")]
         public string UserPassword { get; set; }
 
         public bool CreateNewFTPCredentials { get; set; }
